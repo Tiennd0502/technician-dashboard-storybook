d2cef6972d791c81f04dd2f0138857fd6f0067b4
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-27 15:06:42 +0700

    Implement ServiceCard component

--- technician-dashboard/src/constants/common.ts
+++ technician-dashboard/src/constants/common.ts
@@ -1,1 +1,11 @@
 export const LOGO_URL = './technician.svg';
+
+export const CURRENCY_CHARACTERS = {
+  EURO: '€',
+};
+
+export const TRUNCATE_STYLE = {
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+};
