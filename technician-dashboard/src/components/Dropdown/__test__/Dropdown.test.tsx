0492efcca2eff330e598a342eb96d5d5f09484a0
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-27 15:28:13 +0700

    Implement Dropdown component

---
+++ technician-dashboard/src/components/Dropdown/__test__/Dropdown.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+
+import { AVATAR_DEFAULT } from '@/constants';
+
+import Dropdown from '../index';
+
+describe('Dropdown test cases', () => {
+  const props = {
+    name: 'John',
+    avatar: AVATAR_DEFAULT,
+  };
+
+  test('should render correctly', () => {
+    const { container } = render(<Dropdown {...props} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('get list options in dropdown component', () => {
+    const { getByTestId } = render(<Dropdown {...props} />);
+
+    const avatar = getByTestId('dropdown-options');
+    expect(avatar).toBeTruthy();
+  });
+});
