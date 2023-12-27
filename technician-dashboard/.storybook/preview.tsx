4a2c547206b02266e8731ae9ab77058d549ed748
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-27 08:40:31 +0700

    Update storybook config

--- technician-dashboard/.storybook/preview.tsx
+++ technician-dashboard/.storybook/preview.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import { ChakraProvider } from '../src/providers';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +12,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <ChakraProvider>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
 };
 
 export default preview;
