f32325fad4faa633a65d1058304163aea7835ebe
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-01-05 09:54:55 +0700

    Update handle search  for product

--- technician-dashboard/app/lib/constants/filter.ts
+++ technician-dashboard/app/lib/constants/filter.ts
@@ -1,9 +1,11 @@
 import { SORT_TYPE } from '@/lib/interfaces';
 
-export const PRODUCT_LIMIT = 5;
+export const PRODUCT_LIMIT = '5';
 
 export const DEFAULT_PRODUCT_FILTER = {
   name: '',
   sortBy: 'name',
   order: SORT_TYPE.Asc,
+  page: '1',
+  limit: PRODUCT_LIMIT,
 };
